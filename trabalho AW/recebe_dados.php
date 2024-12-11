07ab9d9427ef7b326aaafe08c706b5f3cca37634
FranciscoFre1tas <pereira.freitas@escolar.ifrn.edu.br>
2024-12-11 19:13:38 -0300

    Add files via upload

---
+++ trabalho AW/recebe_dados.php
@@ -0,0 +1,12 @@
+<?php
+            echo "<p><strong>Nome:</strong> " . htmlspecialchars($_POST['nome']) . "</p>";
+            echo "<p><strong>Email:</strong> " . htmlspecialchars($_POST['email']) . "</p>";
+            echo "<p><strong>Senha:</strong> " . htmlspecialchars($_POST['senha']) . "</p>";
+            echo "<p><strong>Data de Nascimento:</strong> " . htmlspecialchars($_POST['data_nasc']) . "</p>";
+            echo "<p><strong>Telefone:</strong> " . htmlspecialchars($_POST['telefone']) . "</p>";
+            echo "<p><strong>Site Pessoal:</strong> " . htmlspecialchars($_POST['website']) . "</p>";
+            echo "<p><strong>Cor Favorita:</strong> " . htmlspecialchars($_POST['cor_favorita']) . "</p>";
+            echo "<p><strong>Número Favorito:</strong> " . htmlspecialchars($_POST['numero']) . "</p>";
+            echo "<p><strong>Satisfação:</strong> " . htmlspecialchars($_POST['range']) . "</p>";
+            echo "<p><strong>Gênero:</strong> " . htmlspecialchars($_POST['genero']) . "</p>";
+        ?>
